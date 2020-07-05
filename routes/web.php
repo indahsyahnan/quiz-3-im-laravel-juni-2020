d257ef2a22bf5ae586c9facd50d1a5747c9f2d2a
indahsyahnan <67263728+indahsyahnan@users.noreply.github.com>
2020-07-05 17:15:42 +0700

    Done Quiz-3

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layouts.home');
+});
+
+Route::get('/artikel', 'ArtikelController@index');
+Route::get('/artikel/create', 'ArtikelController@create');
+Route::post('/artikel', 'ArtikelController@store');
+Route::get('/artikel/{id}', 'ArtikelController@show');
+Route::get('/artikel/{id}/edit','ArtikelController@edit');
+Route::put('/artikel/{id}','ArtikelController@update');
+Route::delete('/artikel/{id}','ArtikelController@destroy');
